0556d0eb726cb0e6fe429482e57f8fe0311edb40
Oscar Hong <oscar.hong2015@gmail.com>
2017-04-08 22:02:38 -0400

    took the L

--- R1_PC/R1_PC.py
+++ R1_PC/R1_PC.py
@@ -1,16 +1,53 @@
 # @author: Oscar Hong, Duke University
 
+# helper functions
+
+def chooseStall(stallState):
+    for i in xrange(1,len(stallState)-1):
+        if stallState[i] == 'O':
+            # calculate left distance (LS)
+            LS = 0
+            counter = 0
+            for j in xrange(i, 0, -1):
+                if stallState[i] == 'X':
+                    LS = counter
+                    break
+                else:
+                    counter += 1
+
+            # calculate right distance (RS)
+            RS = 0
+            counter = 0
+            for j in xrange(i,len(stallState)-1):
+                if stallState[i] == 'X':
+                    RS = counter
+                    break
+                else:
+                    counter += 1
+
+
 # main program
 # input() reads a string with a line of input, stripping the '\n' (newline) at the end.
 # This is all you need for most Google Code Jam problems.
-input = open("R1_PB_Input.txt", "r")
-output = open("R1_PB_Output.txt","w")
+input = open("R1_PC_Input.txt", "r")
+output = open("R1_PC_Output.txt","w")
 
 t = int(input.readline())
 
 for i in range(1, t + 1):
-  n = int(input.readline())
-  lastTidyNum = getLastTidyNum(n)
-  output.write("Case #{}: {}\n".format(i, lastTidyNum))
+  n, k = [int(s) for s in input.readline().split(" ")] # N stalls; K ppl
+
+  # init stall state
+  stallState = []
+  for i in xrange(n+2):
+      if i == 0 or i == n+1:
+          stallState.append('X')
+      else:
+          stallState.append('O')
+
+  for i in xrange(k):
+      chooseStall(stallState)
+
+  # output.write("Case #{}: {} {}".format(i, lastMax, lastMin))
 
 output.close()
